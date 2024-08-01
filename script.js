16247f0808463611d75add272a7bca4f0ab69e37
Jawad-Dheini <bmw530ipad@icloud.com>
2024-08-01 20:30:45 +0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+setTimeout(() => {
+  let typed = new Typed(".multiple-text", {
+    strings: [
+      "Android Developer",
+      "Fullstack Web Developer",
+      "Python Developer",
+      "Machine Learning Enthusiast",
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
+}, 1000);
